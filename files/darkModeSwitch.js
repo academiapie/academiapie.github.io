4aa85aedd86cc40dc62e841a248af451ece87575
drocera <drocera@gmail.com>
2023-01-07 15:18:38 +0100

    Updates ChatGPT: darkMode i HamburgerMenu

--- files/darkModeSwitch.js
+++ files/darkModeSwitch.js
@@ -24,7 +24,7 @@ function toggleDarkMode() {
   }
 }
 
-function initDarkMode() {
+/*function initScreenModeSwitch() {
   let darkMode = localStorage.getItem("darkMode");
   if (darkMode === "true") {
     darkModeSwitches.forEach((item) => item.classList.add("dark"));
@@ -34,10 +34,34 @@ function initDarkMode() {
     darkModeSwitches.forEach((item) => item.classList.add("light"));
     return;
   }
-  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    document.documentElement.setAttribute("data-mode", "dark");
-    darkModeSwitches.forEach((item) => item.classList.add("dark"));
+  if (window.matchMedia) {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      document.documentElement.setAttribute("data-mode", "dark");
+      darkModeSwitches.forEach((item) => item.classList.add("dark"));
+    }
+    else {
+      document.documentElement.setAttribute("data-mode", "light");
+      darkModeSwitches.forEach((item) => item.classList.add("light"));
+    }
+  }
+}*/
+
+/* Chat GPT suggestion:*/
+function initScreenModeSwitch() {
+  let darkMode = localStorage.getItem("darkMode");
+  /*Chat GPT debugging:
+  const parentElement = document.querySelector('#debugRef');
+  const p = document.createElement('p');
+  p.innerHTML = `darkMode: ${localStorage.getItem('darkMode')}`;
+  parentElement.prepend(p);
+  */
+  if (darkMode === null) {
+    if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      localStorage.setItem("darkMode", true);
+    } else {
+      localStorage.setItem("darkMode", false);
+    }
   }
 }
 
-initDarkMode();
+initScreenModeSwitch();
